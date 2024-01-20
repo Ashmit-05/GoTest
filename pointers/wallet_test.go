ef3cf9700cef84648ce5f797ca99dc35190f8884
Ashmit05 <ashmitbhardwaj05@gmail.com>
2024-01-20 06:54:26 +0530

    refactor

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,41 +3,56 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T)  {
+  t.Run("deposit", func(t *testing.T) {
+    wallet := Wallet{}
+    wallet.Deposit(Bitcoin(10))
 
-  assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+    assertBalance(t, wallet, Bitcoin(10))
+  })
+
+  t.Run("withdraw with funds", func(t *testing.T) {
+    wallet := Wallet{bitcoin: Bitcoin(10)}
+    err := wallet.Withdraw(Bitcoin(5))
+
+    assertNoError(t,err)
+    assertBalance(t, wallet, Bitcoin(5))
+  })
+
+  t.Run("withdraws insufficient funds", func(t *testing.T) {
+    wallet := Wallet{bitcoin: Bitcoin(10)}
+    err := wallet.Withdraw(Bitcoin(50))
+
+    assertError(t,err,ErrInsufficientFunds)
+    assertBalance(t, wallet, Bitcoin(10))
+  })
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
     t.Helper()
     got := wallet.Balance()
     if got != want {
       t.Errorf("got %s, want %s", got, want)
     }
+  
+}
+
+func assertError(t testing.TB, got, want error) {
+  t.Helper()
+  
+  if got == nil {
+    t.Fatal("wanted an error but didn't get one")
   }
 
-  assertError := func (t testing.TB,got error, want string) {
-    t.Helper()
-    if got == nil {
-      t.Fatal("didn't get an error but wanted one")
-    }
-
-    if got.Error() != want {
-      t.Errorf("got %q want %q", got, want)
-    }
-    
+  if got != want {
+    t.Errorf("got %q, want %q", got, want)
   }
+  
+}
 
-  t.Run("deposit", func(t *testing.T) {
-    wallet := Wallet{}
+func assertNoError(t testing.TB, got error) {
+  t.Helper()
 
-    wallet.Deposit(Bitcoin(10))
-    assertBalance(t, wallet, Bitcoin(10))
-
-  })
-  t.Run("withdraw", func(t *testing.T) {
-    wallet := Wallet{bitcoin: Bitcoin(10)}
-
-    err := wallet.Withdraw(Bitcoin(5))
-
-    assertError(t, err, "Not enough balance")
-    assertBalance(t, wallet, Bitcoin(5))
-
-  })
+  if got != nil {
+    t.Fatal("got an error but didn't want one")
+  }
 }
