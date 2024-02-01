bd998ba5e4ed5553d21a13e47fc417c344f57b08
Ashmit05 <ashmitbhardwaj05@gmail.com>
2024-02-01 17:58:29 +0530

    dasdf

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 
 	cases := []struct {
@@ -35,7 +45,15 @@ func TestWalk(t *testing.T) {
       }{"Chris", 33},
       []string{"Chris"},
     },
-	}
+	  {
+      "nested fields",
+      Person{
+        "Chris",
+        Profile{33, "London"},
+      },
+      []string{"Chris", "London"},
+    },
+  }
 
 	for _, test := range cases {
 		t.Run(test.Name, func(t *testing.T) {
